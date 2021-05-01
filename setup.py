fecc62a4cebd9980c33e3839cae308ee63cd3651
rajatbisoi1 <rajatbisoi1@gmail.com>
2021-05-01 22:37:53 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='compelete sentiment analysis with mlops',
+    author='RajaTBisoI',
+    license='MIT',
+)
